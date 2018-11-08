406b5117a5eb83020a6a8d86b4188ddb41c69488
Drackonis <kergastel@hotmail.fr>
2018-11-08 15:56:01 +0100

    libft

--- LIBFT/strdup.c
+++ LIBFT/strdup.c
@@ -2,5 +2,21 @@
 
 char	*ft_strdup(char *src)
 {
-	
+	int	i;
+	int	j;
+	char	*c;
+
+	i = 0;
+	while (src[i])
+		i++;
+	if (!(c = malloc(sizeof(*c) * i)
+		return (NULL);
+	j = 0;
+	while (j < i)
+	{
+		c[j] = src[j];
+		j++;
+	}
+	c[j] = '\0';
+	return (c);
 }
