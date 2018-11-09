040c218f8e8c88241535f8252cfb8873a647d9ff
Romain <kergastel@hotmail.fr>
2018-11-09 16:55:10 +0100

    libft

--- LIBFT/strdup.c
+++ LIBFT/strdup.c
@@ -1,15 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strdup.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkergast <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/09 16:53:14 by rkergast          #+#    #+#             */
+/*   Updated: 2018/11/09 16:54:39 by rkergast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
 char	*ft_strdup(char *src)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
 	char	*c;
 
 	i = 0;
 	while (src[i])
 		i++;
-	if (!(c = malloc(sizeof(*c) * i)
+	if (!(c = malloc(sizeof(*c) * i)))
 		return (NULL);
 	j = 0;
 	while (j < i)
